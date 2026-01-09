04cd8240481c41fdb0bbb73dc4ea5b05193e358b
Ishan Mahajan <im530@cam.ac.uk>
2026-01-09 16:32:19 +0000

    Responsive Nav

---
+++ public/index.js
@@ -0,0 +1,10 @@
+// var menu = document.querySelector("menu");
+var burger = document.querySelector("#burger");
+
+burger.addEventListener("click", () => {
+  if (menu.classList.contains("hidden")) {
+    menu.classList.remove("hidden");
+  } else {
+    menu.classList.add("hidden");
+  }
+});
